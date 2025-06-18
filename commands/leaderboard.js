1209429bfc68cc84d27804a66905c00aa400c4f9
pkd2210 <pkd2210@users.noreply.github.com>
2025-06-19 01:22:23 +0300

    Add leaderboard command and update configuration for leaderboard limit
    and add README

---
+++ commands/leaderboard.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js');
+const config = require('../config.json');
+
+//command setup
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('leaderboard')
+		.setDescription(`Top ${config.leaderboard_limit} users with the most balance.`),
+  // command execution
+	async execute(interaction, userdb) {
+		userdb.all('SELECT id, balance FROM users ORDER BY balance DESC LIMIT ?', [config.leaderboard_limit], (err, rows) => {
+			if (err) {
+				return interaction.reply({ content : 'An error occurred while fetching the leaderboard.', flags: MessageFlags.Ephemeral });
+			}
+			if (rows.length === 0) {
+				return interaction.reply({ content: 'No users found in the leaderboard.', flags: MessageFlags.Ephemeral });
+			}
+			const embed = new EmbedBuilder()
+				.setColor(0x008080)
+				.setTitle('Leaderboard')
+				.setDescription(`Top ${config.leaderboard_limit} users with the most balance:`)
+				.setThumbnail(interaction.guild.iconURL())
+				.setTimestamp();
+			rows.forEach((row, index) => {
+				const user = interaction.guild.members.cache.get(row.id) || { user: { username: 'Unknown User', displayAvatarURL: () => '' } };
+				embed.addFields({
+					name: `${index + 1}. ${user.user.username}`,
+					value: `💰 Balance: **${row.balance}**`,
+					inline: false
+				});
+			});
+			interaction.reply({ embeds: [embed] });
+		});
+	}
+};
